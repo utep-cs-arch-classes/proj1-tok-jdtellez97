e38f28beaf753acff292be253155f0347b16d516
student <jdtellez@miners.utep.edu>
2021-03-08 01:10:44 -0700

    Added tokenize function in tokenizer.c.

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -3,17 +3,17 @@
 #include"tokenizer.h"
 
   int space_char(char c){
-  if((c =='\t' || c == '\n') && c == ' ')
+  if((c =='\t' || c == ' ')&& c != 0 )
     return 1;
   else  return 0;
   }
   int non_space_char(char c){
-  if((c != '\t' || c !='\n') && c != ' ')
+  if((c != '\t' || c !=' ') && c != 0 )
    return 1;
    else return 0;
    }
 
-  char *word_start (char *str){
+  char *word_start(char *str){
     while(*str){
       if(space_char(*str))
 	 *str++;
@@ -28,21 +28,17 @@
     }
        return str;
     }
-
   int count_words(char *str){
-  int counter,  state ;
-  while(*str){
-    if(space_char(*str)){
-      state = 0;
-       *str++;
+    int c = 0;
+    char *temp = str;
+  while(*temp){
+    temp = word_start(temp);
+    temp = word_end(temp);
+    c++;
     }
-    else if(non_space_char(*str)){
-      state = 1;
-    counter++;
-    }
-     return counter;
+     return c;
   }
-}
+
   char *copy_str(char *inStr, short len){
   int i;
   char *c = malloc(sizeof(*inStr)+1);
@@ -51,13 +47,12 @@
   }
   return c;
 }
-
   void print_tokens(char ** tokens){
   int i = 0;
   while(*(tokens+i)){
     printf("[%d]: %s ", i, *(tokens+i));
 	   i++;
-      }
+     }
 }
   void free_tokens(char ** tokens ){
   int i = 0;
@@ -67,17 +62,31 @@
   }
   free(tokens);
 }
-  
-		      
-int main(){
+ char **tokenize(char *str){
+   
+    int c = 0;
+    int words = count_words(str);
+     char *temp = str;
+    char **tokens = (char**) malloc(sizeof(char*)*(words+1));
 
-  char * c = "Leroy Sinclair";
-  // printf("\n");
-  // printf("The first char is \n  %c ",*word_start(c));
-  // printf("\n");
-  // printf("terminator triggered.\n  %c ",*word_end(c));
-  printf("words in total: %d\n ",count_words(c));
+    while(c < words){
+      temp = word_start(temp);
+      char *current = copy_str(temp, (word_end(temp) - word_start(temp)));
+      temp = word_end(temp);
+      *(tokens+c)  = current;
+      c++;
+    }
+    *(tokens+words) = '\0';
+    return tokens;
+}   
+int main(){
+  char * c = "Derp";
+   // printf("\n");
+   // printf("The first char is \n  %c ",*word_start(c));
+   // printf("\n");
+   // printf("terminator triggered.\n  %c ",*word_end(c));
+   // printf("\n");
+   // printf("words in total: %s \n ",tokenize(c));
+   // printf("\n");
   return 0;
 }
-
-
