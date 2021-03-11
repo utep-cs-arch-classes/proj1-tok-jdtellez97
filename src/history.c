0cbb11aa589d7b5e6b4933fbfbaa0c6cef48ef58
student <jdtellez@miners.utep.edu>
2021-03-10 18:19:15 -0700

    fixed some errors in history.c

--- src/history.c
+++ src/history.c
@@ -2,17 +2,17 @@
 #include "malloc.h"
 #include "history.h"
 
-int c = 0; \* serves as a counter for id*/
+int c = 0; /* serves as a counter for id*/
 List *init_history(){
   List *head = (List*)malloc(sizeof(List));
-  head->root = null;
+  head->root = NULL;
   return head;
 }
 
 void add_history(List *list, char *str){
   Item *temp = list->root;
   if(list->root == NULL){
-    list->root = (*Item)malloc(sizeof(Item));
+    list->root = (Item*)malloc(sizeof(Item));
     list->root->id = c++;
     list->root->str = str;
     list->root->next = NULL;
@@ -20,7 +20,7 @@ void add_history(List *list, char *str){
     while(temp->next != NULL){
       temp = temp->next;
     }
-      temp->next = (*Item)malloc(sizeof(Item));
+      temp->next = (Item*) malloc(sizeof(Item));
       temp->next->id =  c++;
       temp->next->str = str;
       temp->next->next = NULL;
@@ -29,12 +29,12 @@ void add_history(List *list, char *str){
 char *get_history(List *list, int id){
   Item *temp = list-> root;
   while(temp != NULL){
-    if(temp-> id == id) return tmp->str;
+    if(temp-> id == id) return temp->str;
     temp = temp->next;
   }
 }
 void print_history(List *list){
-  Itemp *temp = list->root;
+  Item *temp = list->root;
   while(temp != NULL){
     printf("%s ", temp->str);
   }
