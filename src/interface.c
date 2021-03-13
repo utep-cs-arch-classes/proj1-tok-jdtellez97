6e86a1fab6e4f381c1c3a43339676e7e2cc2e2bf
student <jdtellez@miners.utep.edu>
2021-03-12 17:25:10 -0700

    added some features that are buggy

--- src/interface.c
+++ src/interface.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
+#include "tokenizer.h"
+#include "history.h"
 #define LIMIT 50
 
 int main(){
 
   char s[50];
-  int i,c;
+  char ** tokens;
+    int i ,c, id;
+  List *list = init_history();
 
   i = 0;
   while(i != LIMIT-1){
@@ -13,8 +17,15 @@ int main(){
       break;
     s[i++] = c;
   }
-  s[i] = '\0';
+   s[i] = '\0';
   printf("You entered: %s\n",s);
+  // printf("Tokenising...\n ");
+  //tokens =  tokenize(s);
+  //printf("Total number of words : %d \n ", copy_str(s, get_length(s)));
+  //printf(" Your tokens are : %s \n" );
+  //print_tokens(tokens);
+  //free_tokens(tokens);
+  
   return 0;
 
 }
